b0fa5f6c2f9c0df7a7854ad9873e78ab0dc1d79c
Jesus Francisco De Leon Cisneros <al188469@edu.uaa.mx>
2022-09-06 19:40:22 -0500

    Adding a DbContext Class

---
+++ AppCitas/AppCitas/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using AppCitas.Ententies;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppCitas.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+    public DbSet<AppUser> Users { get; set; }
+}
