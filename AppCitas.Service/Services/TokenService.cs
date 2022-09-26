291b9a500328fc2a1f508ae68e460318afbdad6c
Jesus Francisco De Leon Cisneros <al188469@edu.uaa.mx>
2022-09-25 23:30:26 -0500

    Adding a token service

---
+++ AppCitas.Service/Services/TokenService.cs
@@ -0,0 +1,14 @@
+﻿using AppCitas.Service.Entities;
+using AppCitas.Service.Interfaces;
+
+namespace AppCitas.Service.Services
+{
+    public class TokenService: ITokenService
+    {
+        public string CreateToken(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
