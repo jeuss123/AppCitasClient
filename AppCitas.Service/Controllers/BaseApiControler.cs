9aebac4e41a994f6ab1c6b94b66cc3d02e49c5ca
Jesus Francisco De Leon Cisneros <al188469@edu.uaa.mx>
2022-11-29 20:36:07 -0600

    Ultima actulización del proyecto

--- AppCitas.Service/Controllers/BaseApiControler.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace AppCitas.Service.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-public class BaseApiControler: ControllerBase
-{
-}
--- AppCitas.Service/Data/Migrations/DataContextModelSnapshot.cs
+++ AppCitas.Service/Data/Migrations/DataContextModelSnapshot.cs
@@ -67,6 +67,48 @@ namespace AppCitas.Service.Data.Migrations
                     b.ToTable("Users");
                 });
 
+            modelBuilder.Entity("AppCitas.Service.Entities.Message", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("INTEGER");
+
+                    b.Property<string>("Content")
+                        .HasColumnType("TEXT");
+
+                    b.Property<DateTime?>("DateRead")
+                        .HasColumnType("TEXT");
+
+                    b.Property<DateTime>("MessageSent")
+                        .HasColumnType("TEXT");
+
+                    b.Property<bool>("RecipientDeleted")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<int>("RecipientId")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<string>("RecipientUsername")
+                        .HasColumnType("TEXT");
+
+                    b.Property<bool>("SenderDeleted")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<int>("SenderId")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<string>("SenderUsername")
+                        .HasColumnType("TEXT");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("RecipientId");
+
+                    b.HasIndex("SenderId");
+
+                    b.ToTable("Messages");
+                });
+
             modelBuilder.Entity("AppCitas.Service.Entities.Photo", b =>
                 {
                     b.Property<int>("Id")
@@ -92,6 +134,40 @@ namespace AppCitas.Service.Data.Migrations
                     b.ToTable("Photos");
                 });
 
+            modelBuilder.Entity("AppCitas.Service.Entities.UserLike", b =>
+                {
+                    b.Property<int>("SourceUserId")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<int>("LikedUserId")
+                        .HasColumnType("INTEGER");
+
+                    b.HasKey("SourceUserId", "LikedUserId");
+
+                    b.HasIndex("LikedUserId");
+
+                    b.ToTable("Likes");
+                });
+
+            modelBuilder.Entity("AppCitas.Service.Entities.Message", b =>
+                {
+                    b.HasOne("AppCitas.Service.Entities.AppUser", "Recipient")
+                        .WithMany("MessagesReceived")
+                        .HasForeignKey("RecipientId")
+                        .OnDelete(DeleteBehavior.Restrict)
+                        .IsRequired();
+
+                    b.HasOne("AppCitas.Service.Entities.AppUser", "Sender")
+                        .WithMany("MessagesSent")
+                        .HasForeignKey("SenderId")
+                        .OnDelete(DeleteBehavior.Restrict)
+                        .IsRequired();
+
+                    b.Navigation("Recipient");
+
+                    b.Navigation("Sender");
+                });
+
             modelBuilder.Entity("AppCitas.Service.Entities.Photo", b =>
                 {
                     b.HasOne("AppCitas.Service.Entities.AppUser", "AppUser")
@@ -103,8 +179,35 @@ namespace AppCitas.Service.Data.Migrations
                     b.Navigation("AppUser");
                 });
 
+            modelBuilder.Entity("AppCitas.Service.Entities.UserLike", b =>
+                {
+                    b.HasOne("AppCitas.Service.Entities.AppUser", "LikedUser")
+                        .WithMany("LikedByUsers")
+                        .HasForeignKey("LikedUserId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("AppCitas.Service.Entities.AppUser", "SourceUser")
+                        .WithMany("LikedUsers")
+                        .HasForeignKey("SourceUserId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("LikedUser");
+
+                    b.Navigation("SourceUser");
+                });
+
             modelBuilder.Entity("AppCitas.Service.Entities.AppUser", b =>
                 {
+                    b.Navigation("LikedByUsers");
+
+                    b.Navigation("LikedUsers");
+
+                    b.Navigation("MessagesReceived");
+
+                    b.Navigation("MessagesSent");
+
                     b.Navigation("Photos");
                 });
 #pragma warning restore 612, 618
